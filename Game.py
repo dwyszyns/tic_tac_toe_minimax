41c06d917578a9d717aeee8b732900bcdb72ea8e
Dominika <01168920@pw.edu.pl>
2023-11-29 21:38:32 +0100

    sth changed

--- Game.py
+++ Game.py
@@ -33,8 +33,9 @@ class Game:
 
 if __name__ == "__main__":
     board = Board(3)
+    print("Starting game...")
     player1 = AIPlayer("x", "Dominika", 9)
-    player2 = AIPlayer("o", "Wojtek", 9)
+    player2 = AIPlayer("o", "Wojtek", 1)
     game = Game(board, player1, player2)
     game.play()
     game.print_game_result()
--- Player.py
+++ Player.py
@@ -2,45 +2,51 @@ from cmath import inf
 from Board import Board
 import random
 
-warunki = {
-    True: lambda current_value, value, current_depth, best_depth: current_value > value
-    or (current_value == value and current_depth < best_depth),
-    False: lambda current_value, value, current_depth, best_depth: current_value < value
-    or (current_value == value and current_depth < best_depth),
-}
 
 class AIPlayer:
-    def __init__(self, symbol:str, name:str, depth:int):
+    def __init__(self, symbol: str, name: str, depth: int):
         self.symbol = symbol
         self.name = name
-        self.depth = depth 
+        self.depth = depth
 
-    def move(self, board:Board):
+    def move(self, board: Board):
         value, move, _ = self.minimax(board, self.depth, True)
         print(f"Chosen: {value} {move}")
         board.board[move[0]][move[1]] = self.symbol
 
-    def evaluate(self, board:Board) -> int:
+    def evaluate(self, board: Board) -> int:
         price_board = board.generate_price_board()
         price = 0
-        oponent_symbol = "x" if self.symbol == "o" else "o"
+        opponent_symbol = "x" if self.symbol == "o" else "o"
         for i in range(board.board_size):
             for j in range(board.board_size):
                 if board.board[i][j] == self.symbol:
                     price += price_board[i][j]
 
-                elif board.board[i][j] == oponent_symbol:
+                elif board.board[i][j] == opponent_symbol:
                     price -= price_board[i][j]
 
         if board.check_winner() == self.symbol:
             price += 50
 
-        elif board.check_winner() == oponent_symbol:
+        elif board.check_winner() == opponent_symbol:
             price -= 50
 
         return price
 
-    def minimax(self, board:Board, depth:int, maximizing_player:bool =True):
+    def check_best_move_condition(
+        self, current_value, value, current_depth, best_depth, maximizing_player
+    ):
+        if maximizing_player:
+            return current_value > value or (
+                current_value == value and current_depth < best_depth
+            )
+        else:
+            return current_value < value or (
+                current_value == value and current_depth < best_depth
+            )
+
+    def minimax(self, board: Board, depth: int, maximizing_player: bool = True):
         if depth == 0 or board.game_over() or board.check_winner() is not None:
             return self.evaluate(board), None, depth
 
@@ -51,11 +57,15 @@ class AIPlayer:
         best_move = None
         opponent_symbol = "o" if self.symbol == "x" else "x"
         for move in available_moves:
-            board.board[move[0]][move[1]] = (self.symbol if maximizing_player else opponent_symbol)
-            current_value, _, current_depth = self.minimax(board, depth - 1, not maximizing_player)
+            board.board[move[0]][move[1]] = (
+                self.symbol if maximizing_player else opponent_symbol
+            )
+            current_value, _, current_depth = self.minimax(
+                board, depth - 1, not maximizing_player
+            )
             board.board[move[0]][move[1]] = " "
-            if warunki[maximizing_player](
-                current_value, value, current_depth, best_depth
+            if self.check_best_move_condition(
+                current_value, value, current_depth, best_depth, maximizing_player
             ):
                 value = current_value
                 best_move = move
